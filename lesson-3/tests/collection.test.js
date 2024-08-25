86b299d61498139a1653cc8440a4a43e59e38bba
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2024-08-25 17:07:09 +0300

    finish lesson-3

--- lesson-3/tests/collection.test.js
+++ lesson-3/tests/collection.test.js
@@ -5,6 +5,25 @@ const get = getFunction();
 
 // BEGIN (write your solution here)
 
+if (get({foo: 'bar'}, 'foo') !== 'bar') {
+  throw new Error('case 1');
+}
+
+
+if (get({foo: 'bar'}, 'baz') !== undefined) {
+  throw new Error('case 2');
+}
+
+
+if (get({foo: 'bar'}, 'baz', 'value') !== 'value') {
+  throw new Error('case 3');
+}
+
+if (get({foo: 'bar'}, 'foo', 'value') !== 'bar') {
+  throw new Error('case 4');
+}
+
+
 
 
 // END
