2af231f4573251a9e3a6aa1c17c219218842a443
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2024-08-25 17:48:40 +0300

    finish practice lesson-05

--- lesson-05-jest/practice/__tests__/without.test.js
+++ lesson-05-jest/practice/__tests__/without.test.js
@@ -1,9 +1,14 @@
 // @ts-check
 
 import getFunction from "../functions.js";
-
 const without = getFunction();
 
 // BEGIN (write your solution here)
+
+test("without", () => {
+  expect(without([1, 2, 3, 4, 5], 1, 2, 3, 4)).toEqual([5]);
+  expect(without([1, 2, 3, 4, 4, 5], 1, 2, 3, 4)).toEqual([5]);
+  expect(without([1, 2, 3, 4, 5], 1, 2, 3, 4, 5)).toEqual([]);
+});
 
 // END
