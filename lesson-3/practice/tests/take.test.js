a28174201d47340827375efda1ac72444896e52b
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2024-08-25 17:30:27 +0300

    finish lesson-3 practice

--- lesson-3/practice/tests/take.test.js
+++ lesson-3/practice/tests/take.test.js
@@ -4,5 +4,14 @@ import getFunction from "../functions.js";
 const take = getFunction();
 
 // BEGIN (write your solution here)
-
+assert.strict.deepEqual(take([1, 2, 3]), [1]);
+assert.strict.deepEqual(take([1, 2, 3], 100), [1, 2, 3]);
+assert.strict.deepEqual(take([1, 2, 3], 3), [1, 2, 3]);
+assert.strict.deepEqual(take([1, 2, 3], 2), [1, 2]);
+assert.strict.deepEqual(take([1, 2, 3], 1), [1]);
+assert.strict.deepEqual(take([1, 2, 3], 0), []);
+assert.strict.deepEqual(take([], 100), []);
+assert.strict.deepEqual(take([], -100), []);
+assert.strict.deepEqual(take([1, 2, 3], -100), []);
+assert.strict.deepEqual(take([1, 2, 3], -1), []);
 // END
